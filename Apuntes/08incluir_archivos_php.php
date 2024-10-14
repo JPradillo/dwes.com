468dc916b7f0633ceb50748ac9532908d2ca9c28
JPradillo <145434802+JPradillo@users.noreply.github.com>
2024-10-14 11:41:17 +0200

    Add files via upload

--- Apuntes/08incluir_archivos_php.php
+++ Apuntes/08incluir_archivos_php.php
@@ -19,12 +19,18 @@
     */
     $variable = 8;
 
-    // Direcotorio raíz del servidor: /var/www/dwes.com
-    $_SERVER['DOCUMENT_ROOT'];
-    
-    include("includes/funciones.php");
 
-    inicio_html("Inclusión de archivos", ["estilos/general.css"]);
+    // Incluir archivos modificando el include_path de php.ini
+    // $include_path_actual = ini_get("include_path");  // Leo el valor actual de include_path
+    // $include_path_actual .= ":" . $_SERVER['DOCUMENT_ROOT'] . "/Apuntes/includes";  // Añado el directorio
+    // ini_set("include_path", $include_path_actual);  // Asigno el nuevo include_path
+
+    // Direcotorio raíz del servidor: /var/www/dwes.com
+    // require("funciones.php");  // Solo tengo que poner el nombre del archivo
+
+    // Modificar archivos sin modificar el include_path y poniendo la ruta absoluta
+    require($_SERVER['DOCUMENT_ROOT'] . "/Apuntes/includes/funciones.php");
+    inicio_html("Inclusión de archivos", ["/Apuntes/estilos/general.css"]);
 
     echo "<h1>Inclusión de archivos php en scripts PHP</h1>";
     fin_html();
